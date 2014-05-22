71cc17d7466f227479f11b513ebad1288ef65854
HilbertYu <hsinshsuanyu@gmail.com>
2014-05-22 18:36:17 +0800

    huffman tree

--- main.cpp
+++ main.cpp
@@ -133,6 +133,22 @@ void ff(Node *node, encoding_t* encoding_data, encoding_t* e_table) {
     }
 
 }
+
+int bit_add(uchar* des, const uchar* src, int start_bit, int num_bit) {
+    unsigned int bit_cur= 0;
+    for (int i = 0; i <  num_bit; ++i) {
+        bit_cur = (start_bit + i) % 8;
+//        des[(i + start_bit)>>3] |= (src[i >> 8] & (0x1 << (i % 8))) << bit_cur;
+        des[(i + start_bit)>>3] |= ((src[i >> 8] >> (i % 8)) & 0x1 ) << bit_cur;
+//        printf("((%d))\n", (i+start_bit)>>3);
+        printf("bit_cur = %d\n", bit_cur);
+        //printf("--%d--\n",(src[i >> 8] & (0x1 << i)) << bit_cur);
+        printf("--%d--\n", (src[i >> 8] >> (i % 8)) & 0x1 );
+    }
+
+    return start_bit + num_bit;
+}
+
 /*===========================================================================*/
 int main(void) {
 
@@ -159,7 +175,7 @@ int main(void) {
     //
 
     Node * p = setup_huffman_tree(forest);
-    print_postfix(p);
+    //print_postfix(p);
 
 
     encoding_t encoding_table[256];
@@ -167,20 +183,50 @@ int main(void) {
     encoding_t e_data = {0,0};
     ff(p, &e_data, encoding_table);
 
-    for(int i = 0; i < 256; ++i) {
-        if (encoding_table[i].len) {
-            printf("coding[%d] = [%u]", i, encoding_table[i].len);
-            for(int j = 0; j < encoding_table[i].len; ++j) {
-                printf("%u-", (encoding_table[i].data >> j) & 0x1);
-                //                printf("[%u-]", (encoding_table[i].data >> encoding_table[i].len));
-            }
-            printf("\n");
+    // for(int i = 0; i < 256; ++i) {
+    //     if (encoding_table[i].len) {
+    //         printf("char[%c], weight[%d]  ", i, weight_table[i]);
+    //         printf("coding[%d] = [%u]", i, encoding_table[i].len);
+    //         for(int j = 0; j < encoding_table[i].len; ++j) {
+    //             printf("%u-", (encoding_table[i].data >> j) & 0x1);
+    //             //                printf("[%u-]", (encoding_table[i].data >> encoding_table[i].len));
+    //         }
+    //         printf("\n");
+    //
+    //     }
+    // }
 
-        }
+    uint32_t table_len = 0;
+    for (int i = 0; i < 256; ++i) {
+        if (!encoding_table[i].len)
+            continue;
+
+        table_len += 16 + encoding_table[i].len;
     }
+    table_len += 32;
 
-    printf("encoding_table size = %lu\n", sizeof(encoding_table));
+    printf("table_len (bit) = %d\n", table_len);
+    printf("table_len (byte) = %d\n", table_len/8 + 1);
 
+    /*===========================================================================*/
+    uchar * header_table = new uchar[table_len/8+1];
+
+    delete [] header_table;
+    header_table = NULL;
+    /*===========================================================================*/
+
+    /*===========================================================================*/
+    uchar kk[1024];
+    memset(kk, 0, sizeof(kk));
+    int l = 257;
+    bit_add(kk, (uchar*)&l, 1, 8);
+
+    for(int j = 0; j < 8; ++j) {
+        printf("[%u]", (kk[0] >> j) & 0x1);
+    }
+    printf("\n");
+
+    /*===========================================================================*/
     return 0;
 
 }
